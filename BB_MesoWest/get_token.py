3f1d6e12b8594b8b791e7e39d2d5db9fa8c084ad
Brian Blaylock <blaylockbk@users.noreply.github.com>
2016-11-14 14:52:52 -0700

    Edit this file with your own MesoWest API token

---
+++ BB_MesoWest/get_token.py
@@ -0,0 +1,13 @@
+# Brian Blaylock
+# Version 2 update
+# 14 November 2016
+
+"""
+In an effort to protect my tokens from the public, this function will return
+my MesoWest token as a string, to be used in other functions to make API calls.abs
+
+This file is not uploaded to github, so insert your own token API below.
+"""
+
+def my_token():
+    return 'INSERT_YOUR_TOKEN_HERE'
