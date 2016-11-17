587693041075c693471511c15e0c48b7bc070289
Brian Blaylock <blaylockbk@gmail.com>
2016-11-17 14:36:06 -0700

    We have liftoff!!!

---
+++ BB_downloads/download_NEXRAD.py
@@ -0,0 +1,14 @@
+# Brian Blaylock
+# Version 2 update
+# 17 November 2016
+
+"""
+Download NEXRAD Level 2 data from the Amazon Web Serice (AWS)
+
+NEXRAD Level 2 data is quite a bit different than TDWR data.
+Download from Amazon Web Service
+https://s3.amazonaws.com/noaa-nexrad-level2/index.html
+
+Download URL Example:
+https://noaa-nexrad-level2.s3.amazonaws.com/2016/08/05/KCBX/KCBX20160805_205859_V06
+"""
