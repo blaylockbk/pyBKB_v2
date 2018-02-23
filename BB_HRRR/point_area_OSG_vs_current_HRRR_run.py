50a49daf48b71d427e2fef3f0eb3c1640493a146
Brian Kenneth Blaylock <u0553130@ad.utah.edu>
2018-02-23 15:11:00 -0700

    updates

--- BB_HRRR/point_area_OSG_vs_current_HRRR_run.py
+++ BB_HRRR/point_area_OSG_vs_current_HRRR_run.py
@@ -7,12 +7,11 @@ Overlay OSG Statistics with current HRRR Runs
 
 import h5py
 import matplotlib.pyplot as plt
+from mpl_toolkits.basemap import Basemap
 import numpy as np
 from collections import OrderedDict
 from datetime import datetime, timedelta
 import multiprocessing
-
-from mpl_toolkits.basemap import Basemap
 
 import sys
 sys.path.append('/uufs/chpc.utah.edu/common/home/u0553130/pyBKB_v2')
@@ -170,10 +169,202 @@ def plot_for_each_fxx(f):
     plt.close()
     return None
 
+def plot_for_each_fxx_with_Map(f):   
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=[15,6])
+    plt.sca(ax1)
+    ## HRRR Model Run date
+    RUN = D-timedelta(hours=f)
+    
+    ## Get HRRR data for this run
+    if var.split(':')[0] == 'UVGRD':
+        # Calculate wind speed from U and V components
+        level = var.split(':')[1]
+        HU = get_hrrr_variable(RUN, 'UGRD:'+level, fxx=f, value_only=True, verbose=False)    
+        HV = get_hrrr_variable(RUN, 'VGRD:'+level, fxx=f, value_only=True, verbose=False)
+        speed = wind_uv_to_spd(HU['value'], HV['value'])
+        H = HU
+        H['value'] = speed
+        HUarea = HU['value'][x-box_radius:x+box_radius+1,y-box_radius:y+box_radius+1]
+        HVarea = HV['value'][x-box_radius:x+box_radius+1,y-box_radius:y+box_radius+1]
+    else:
+        H = get_hrrr_variable(RUN, var, fxx=f, value_only=True, verbose=False)
+    
+    ## Convert units and get values in bounding box area
+    if var == 'TMP:2 m' or var == 'DPT:2 m':
+        H['value'] = H['value']-273.15
+        Hpoint = H['value'][x,y]
+        Harea = H['value'][x-box_radius:x+box_radius+1,y-box_radius:y+box_radius+1]
+    else:
+        Hpoint = H['value'][x,y]
+        Harea = H['value'][x-box_radius:x+box_radius+1,y-box_radius:y+box_radius+1]
+    
+    ## Calculate the HRRR percentiles in the boxed area for this run
+    HP = np.percentile(Harea, [0,25,50,75,100])
+
+    ## --- Plot OSG percentiles ---------------------------------------
+    plt.fill_between(percentiles, area_P[0][0], area_P[0][4],
+                        color='lightgrey',
+                        label='p100-p00',
+                        zorder=1)
+    plt.fill_between(percentiles, area_P[0][1], area_P[0][3],
+                        color='grey',
+                        label='p25-p75',
+                        zorder=1)
+    plt.plot(percentiles, area_P[0][2],
+                color='lightgrey',
+                label="p50",
+                zorder=1)
+    ## Plot all percentiles for each grid box
+    #for i in range(box_radius*2+1):
+    #    for j in range(box_radius*2+1):
+    #        plt.plot(percentiles, PP[i,j,:])
+    
+    ## Plot OSG percentiles for the single grid box nearest the station of interest
+    plt.plot(percentiles, PP[box_radius,box_radius,:],
+                linestyle='--',
+                color='k',
+                label='%s OSG Percentiles' % stn,
+                zorder=1)
+
+    ## --- Plot RUN percentiles -------------------------------------
+    # Linear interpolation to find the percentile for the value
+    for i in range(5):
+        p100Hpoint = HP[i]
+        p100PP = area_P[0][i]
+        if p100Hpoint >= np.max(p100PP):
+            p100Y = p100Hpoint
+            p100X = 100
+            print "Hpoint > PP, exceeds p100", D, f
+        elif p100Hpoint <= np.min(p100PP):
+            p100Y = p100Hpoint
+            p100X = 0
+            print "Hpoint < PP exceeds p00", D, f,
+        else:
+            p100y1 = np.min(filter(lambda x: x>=p100Hpoint, p100PP))
+            p100y1_idx = np.where(p100PP==p100y1)[0][0]
+            p100x1 = percentiles[p100y1_idx]
+            p100y2 = p100PP[p100y1_idx-1]
+            p100x2 = percentiles[p100y1_idx-1]
+            p100m = (p100y2-p100y1)/(p100x2-p100x1)
+            p100b = 0-p100m*p100x1+p100y1
+            p100Y = p100Hpoint
+            p100X = (p100Y-p100b)/p100m
+        plt.axhline(p100Hpoint, xmax=p100X/100, color='r', linewidth=1, zorder=50)
+        plt.axvline(p100X, ymax=(p100Hpoint-ymin)/(ymax-ymin), color='r', linewidth=1, zorder=50)
+        plt.scatter(p100X, p100Hpoint, s=25, color='r', zorder=50)
+    ## Plot value from current HRRR run for every grid box
+    #for i in range(box_radius*2+1):
+    #    for j in range(box_radius*2+1):
+    #        plt.axhline(Harea[i,j], linewidth=.1)
+        
+    ## Plot HRRR value for stn grid box for urrent HRRR run
+    # Linear interpolation to find the percentile for the value
+    stnPP = PP[box_radius,box_radius,:]
+    if Hpoint >= np.max(stnPP):
+        Y = Hpoint
+        X = 100
+        print "station Hpoint > PP, exceeds p100", D, f
+    elif Hpoint <= np.min(stnPP):
+        Y = Hpoint
+        X = 0
+        print "station Hpoint < PP, exceeds p00", D, f
+    else:
+        y1 = np.min(filter(lambda x: x>=Hpoint, stnPP))
+        y1_idx = np.where(stnPP==y1)[0][0]
+        x1 = percentiles[y1_idx]
+        y2 = stnPP[y1_idx-1]
+        x2 = percentiles[y1_idx-1]
+        m = (y2-y1)/(x2-x1)
+        b = 0-m*x1+y1
+        Y = Hpoint
+        X = (Y-b)/m
+    plt.axhline(Hpoint, xmax=X/100, color='k', lw=6, zorder=100)
+    plt.axvline(X, ymax=(Hpoint-ymin)/(ymax-ymin), color='k', lw=6, zorder=100)
+    plt.scatter(X, Hpoint, s=150, color='k', zorder=100)
+
+    ## --- Other Plot Elements --------------------------------------
+    plt.grid()
+    plt.legend(loc='upper left')
+
+    plt.xlabel('Percentiles\n%sx%s km box centered at %s' % (np.shape(PP)[0]*3, np.shape(PP)[0]*3, stn))
+    xtick_labels = [0,'','','','',5,10,25,33,50,66,75,90,95,'','','','',100]
+    plt.xticks(percentiles, xtick_labels)
+    plt.xlim([0,100])
+    plt.ylabel(label_units)
+    plt.ylim([ymin, ymax])
+    
+    
+    plt.title('Run: %s F%02d' % (RUN.strftime("%Y-%b-%d %H:%M"), f), loc='left')
+
+    #plt.savefig(SAVEDIR+'%s_f%02d.png' % (D.strftime('%Y-%m-%d_%H'), f))
+    #plt.close()
+
+    #####----- Side Map -------
+    plt.sca(ax2)
+    am.drawcounties()
+    am.arcgisimage(service='World_Shaded_Relief')  
+    
+    if var == 'REFC:entire':
+        dBZ = H['value']
+        dBZ = np.ma.array(dBZ)
+        dBZ[dBZ == -10] = np.ma.masked
+        vmax = 80
+        vmin = 0
+	H['value'] = dBZ
+    elif var == 'UVGRD:10 m' or var == 'UVGRD:80 m':
+        vmax = np.max(Harea)
+        vmin = 0
+    else:
+        vmax = np.max(Harea)
+        vmin = np.min(Harea)
+
+    am.pcolormesh(lon, lat, H['value'], alpha=.3, cmap=cmap, vmax=vmax, vmin=vmin)
+    cb = plt.colorbar(orientation='vertical', pad=0.01, shrink=0.85)
+    cb.set_label(label_units)
+
+    # Station Point
+    am.scatter(MWlon, MWlat)
+    # Bottom Boundary
+    am.drawgreatcircle(LONS_BOX[0,-1], LATS_BOX[0,-1],
+                    LONS_BOX[0,0], LATS_BOX[0,0],
+                    color='k')
+    # Left Boundary
+    am.drawgreatcircle(LONS_BOX[0,0], LATS_BOX[0,0],
+                    LONS_BOX[-1,0], LATS_BOX[-1,0],
+                    color='k')
+    # Top Boundary
+    am.drawgreatcircle(LONS_BOX[-1,-1], LATS_BOX[-1,-1],
+                    LONS_BOX[-1,0], LATS_BOX[-1,0],
+                    color='k')
+    # Right Boundary
+    am.drawgreatcircle(LONS_BOX[-1,-1], LATS_BOX[-1,-1],
+                    LONS_BOX[0,-1], LATS_BOX[0,-1],
+                    color='k')
+    # Barbs
+    if var == 'UVGRD:10 m' or var == 'UVGRD:80 m':
+        am.barbs(LONS_BOX, LATS_BOX, HUarea, HVarea,
+                 zorder=200, length=5, color='k',
+                 barb_increments={'half':2.5, 'full':5,'flag':25},
+                 latlon=True)
+
+    plt.title('Valid: %s' % D.strftime('%Y-%b-%d %H:%M'), loc='right')
+    
+    plt.suptitle('%s  %s' % (stn, var))
+    plt.savefig(SAVEDIR+'%s_f%02d.png' % (D.strftime('%Y-%m-%d_%H'), f))
+    plt.close()
+    return None
+
+###############################################################################
+
+###############################################################################
+
+###############################################################################
 
 ## Set Up
 DATE = datetime(2018, 2, 16, 0)
-stn = 'WBB'
+stn = 'DBSU1'
+percentiles = [0,1,2,3,4,5,10,25,33,50,66,75,90,95,96,97,98,99,100]
+box_radius = 5
 print DATE, stn
 
 # MesoWest Station Info
@@ -194,6 +385,23 @@ y = point[1][0]
 
 print lat[x,y], MWlat, 'and',lon[x,y], MWlon
 
+### Plot with side map
+LATS_BOX = lat[x-box_radius:x+box_radius+1,y-box_radius:y+box_radius+1]
+LONS_BOX = lon[x-box_radius:x+box_radius+1,y-box_radius:y+box_radius+1]
+
+top_right_lat = LATS_BOX.max()
+top_right_lon = LONS_BOX.min()
+
+bot_left_lat = LATS_BOX.min()
+bot_left_lon = LONS_BOX.max()
+
+print bot_left_lon, top_right_lon
+print bot_left_lat, top_right_lat
+
+# Area map
+am = Basemap(llcrnrlon=MWlon-.3,  urcrnrlon=MWlon+.3,
+            llcrnrlat=MWlat-.3, urcrnrlat=MWlat+.3)
+
 ## Area Percentiles and Current HRRR
 
 #var = 'TMP:2 m'
@@ -205,8 +413,6 @@ print lat[x,y], MWlat, 'and',lon[x,y], MWlon
 #var = 'HGT:500'
 
 VARS = ['TMP:2 m', 'UVGRD:10 m', 'DPT:2 m', 'UVGRD:80 m', 'REFC:entire', 'GUST:surface', 'HGT:500']
-
-var = 'TMP:2 m'
 
 for var in VARS:
     print var
@@ -220,33 +426,44 @@ for var in VARS:
     ## OSG data directory
     DIR = '/uufs/chpc.utah.edu/common/home/horel-group2/blaylock/HRRR_OSG/hourly30/%s/' % (variable)
 
-    percentiles = [0,1,2,3,4,5,10,25,33,50,66,75,90,95,96,97,98,99,100]
-    box_radius = 5
-
     sDATE = datetime(2018, 2, 18, 0)
     DATES = [sDATE+timedelta(hours=h) for h in range(24)]
 
     if var == 'TMP:2 m':
         ymin = -20
         ymax = 30
+        cmap = 'Spectral_r'
+        label_units = '2 m Temperature (C)'
     elif var == 'DPT:2 m':
         ymin = -25
         ymax = 15
+        cmap = 'BrBG'
+        label_units = '2 m Dew Point (C)'
     elif var == 'REFC:entire':
         ymin = -10
         ymax = 50
+        cmap = 'gist_ncar'
+        label_units = 'Simulated Reflectivity (dBZ)'
     elif var == 'UVGRD:10 m':
         ymin = 0
         ymax = 20
+        cmap = 'inferno_r'
+        label_units = r'10 m Wind Speed (ms$\mathregular{^{-1}}$)'
     elif var == 'UVGRD:80 m':
         ymin = 0
         ymax = 30
+        cmap = 'magma_r'
+        label_units = r'80 m Wind Speed (ms$\mathregular{^{-1}}$)'
     elif var == 'HGT:500':
         ymin = 5300
         ymax = 5900
+        cmap = 'Blues'
+        label_units = '500 mb Geopotential Height'
     else:
         ymin = 0
         ymax = 20
+        cmap = 'viridis'
+        label_units = 'unknown (unknown)'
 
     for D in DATES:
 
@@ -276,7 +493,15 @@ for var in VARS:
         
         
         ## For this valid date, loop through each forecast hour
+        #p = multiprocessing.Pool(19)
+        #p.map(plot_for_each_fxx, range(19))
+        #p.close()
+
         p = multiprocessing.Pool(19)
-        p.map(plot_for_each_fxx, range(19))
+        p.map(plot_for_each_fxx_with_Map, range(19))
         p.close()
+
+
+        
+
             
